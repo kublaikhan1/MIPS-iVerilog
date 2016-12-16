1f2d0ddfab359dc941d5254326df5a05404fef40
hamad Khawaja <khawajahamad217@gmail.com>
2016-12-16 10:17:34 -0500

    Update trianglesimple.asm

--- trianglesimple.asm
+++ trianglesimple.asm
@@ -9,32 +9,32 @@ end:   .asciiz "Thank you for using the program!"
 .text
 main: 
 
-la $a0, size						   #load address of input 
-li $v0, 4						   # Load 4 into v0, op code to print
+la $a0, size				        #load address of input 
+li $v0, 4				        # Load 4 into v0, op code to print
 syscall 																
 
-la $a0, numStars				           #loads address of space to store input from user 
-li $a1, 8						    #buffer space to store number of 8
+la $a0, numStars				#loads address of space to store input from user 
+li $a1, 8					#buffer space to store number of 8
 li $v0, 5		
 syscall 		 
 
  
 li $s0, 1		 
-move $s1, $v0		        #put read int into s1
+move $s1, $v0		       			#put read int into s1
 li $t0, 0
-li $t1, 0			#t1 holds stars per line 
-li $t2, 1			#t2 holds stars needed per line 
+li $t1, 0				        #t1 holds stars per line 
+li $t2, 1					#t2 holds stars needed per line 
 
-la $a0, star		        #Load star "*" into the a0 address
-li $v0, 4			#Loading op code to print string 
+la $a0, star		       			#Load star "*" into the a0 address
+li $v0, 4					#Loading op code to print string 
 
 triangle:
 beq $t1, $t2,starline 
 bne $t1, $t2 starLoop        	
 							   
 starLoop:			        	
-syscall				#print 1 star 				  	
-addi $t1, $t1, 1 	        #incremenet				  	      	
+syscall						#print 1 star 				  	
+addi $t1, $t1, 1 	        		#incremenet				  	      	
 j triangle	
 
 starline:		      
